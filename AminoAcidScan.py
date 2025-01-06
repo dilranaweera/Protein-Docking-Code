2ef72d2535615d35e5e897958b6dad5b3b194289
drana <dil6655@gmail.com>
2025-01-06 16:35:10 -0600

    sgptaddpasspacktocalcbindenergy

--- AminoAcidScan.py
+++ AminoAcidScan.py
@@ -4,11 +4,13 @@ import optparse
 from rosetta import *
 from rosetta.protocols.scoring import Interface
 from pyrosetta import *
+from pyrosetta.rosetta.protocols import docking
 from pyrosetta import protocols
+from pyrosetta.rosetta.protocols.minimization_packing import PackRotamersMover
 # importing in programs needed for amino acid scanning
 
 init() # (extra options = "-seed ####") also an option
-## import os; os.chdir('.test.output') [may need to comment back in or "activate" if script keeps failing]
+import os; os.chdir('.test.output')
 ## if needed, need to mkdir .test.output
 
 def scanning(pdb_filename, partners, mutant_aa = 'A', 
@@ -21,7 +23,8 @@ def scanning(pdb_filename, partners, mutant_aa = 'A',
         
     dock_jump = 1
     movable_jumps = Vector1([dock_jump])
-    protocols.docking.setup_foldtree(pose, partners, movable_jumps)
+    ## protocols.docking.setup_foldtree(pose, partners, movable_jumps) (could be wrong command)
+    docking.setup_foldtree(pose, partners, movable_jumps)
 
 
     scorefxn = get_fa_scorefxn() #  create_score_function('standard')
@@ -57,7 +60,7 @@ def scanning(pdb_filename, partners, mutant_aa = 'A',
                     filename = pose.pdb_info().name()[:-4] + '_' + pose.sequence()[i-1] + str(pose.pdb_info().number(i)) + '->' + mutant_aa
                 #What is the above line mean
                 ddG_mutants[i] = interface_ddG(pose, i, mutant_aa,
-                    movable_jumps, ddG_scorefxn, interface_cutoff, filename )
+                    movable_jumps, scorefxn, interface_cutoff, filename )
 
         # output results
         print( '='*80 )
@@ -84,7 +87,7 @@ def scanning(pdb_filename, partners, mutant_aa = 'A',
     print( '='*80 )
 
 
-def interface_ddG( pose, mutant_position, mutant_aa, movable_jumps, scorefxn = '',
+def interface_ddG( pose, mutant_position, mutant_aa, movable_jumps, scorefxn = None,
         cutoff = 8.0, out_filename = ''):
     # 1. create a reference copy of the pose
     wt = Pose()    # the "wild-type"
@@ -110,9 +113,9 @@ def interface_ddG( pose, mutant_position, mutant_aa, movable_jumps, scorefxn = '
 
 
     wt_score = calc_binding_energy(wt, scorefxn,
-        mutant_position, cutoff)
+        mutant_position, cutoff, pack_scorefxn)
     mut_score = calc_binding_energy(mutant, scorefxn,
-        mutant_position, cutoff)
+        mutant_position, cutoff, pack_scorefxn)
 
     ddg = mut_score - wt_score
 
@@ -131,7 +134,7 @@ def interface_ddG( pose, mutant_position, mutant_aa, movable_jumps, scorefxn = '
 
 
 def mutate_residue(pose, mutant_position, mutant_aa,
-        pack_radius = 0.0, pack_scorefxn = '' ):
+        pack_radius = 0.0, pack_scorefxn = None):
 
 
 
@@ -142,8 +145,12 @@ def mutate_residue(pose, mutant_position, mutant_aa,
     test_pose.assign(pose)
 
 
-    if not pack_scorefxn:
-        pack_scorefxn = get_fa_scorefxn() #  create_score_function('standard')
+    ##if not pack_scorefxn:
+        ## pack_scorefxn = get_fa_scorefxn() #  create_score_function('standard') (these two lines in original script)
+    if pack_scorefxn is None:
+        pack_scorefxn = get_fa_scorefxn()
+
+
 
     task = standard_packer_task(test_pose)
 
@@ -169,13 +176,17 @@ def mutate_residue(pose, mutant_position, mutant_aa,
             task.nonconst_residue_task(i).prevent_repacking()
 
 
-    packer = protocols.simple_moves.PackRotamersMover(pack_scorefxn, task)
+    ## packer = protocols.simple_moves.PackRotamersMover(pack_scorefxn, task) from original script
+    packer = PackRotamersMover(pack_scorefxn, task)
     packer.apply(test_pose)
 
     return test_pose
 
 def calc_binding_energy(pose, scorefxn, center, cutoff = 8.0):
     # create a copy of the pose for manipulation
+    if pack_scorefxn is None: 
+        pack_scorefxn = get_fa_scorefxn()
+
     test_pose = Pose()
     test_pose.assign(pose)
 
@@ -198,7 +209,8 @@ def calc_binding_energy(pose, scorefxn, center, cutoff = 8.0):
     tf.push_back(prevent_repacking)
 
     # setup a PackRotamersMover
-    packer = protocols.simple_moves.PackRotamersMover(scorefxn)
+    ## packer = protocols.simple_moves.PackRotamersMover(scorefxn) (in original script)
+    packer = PackRotamersMover(pack_scorefxn)
     packer.task_factory(tf)
 
 
