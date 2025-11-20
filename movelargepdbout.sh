f3c59fa00f004fb2136f1b49c99e03959366c115
drana <dil6655@gmail.com>
2025-11-20 04:55:52 -0600

    deletedscript

---
+++ movelargepdbout.sh
@@ -0,0 +1,5 @@
+## Script info: 
+## Moves larger, combined pdb files out from ADCP docked results
+
+INPUT_DIR="/Users/dilrana/Desktop/Kuczera/ADCPtop100outputfiles11.17.25"
+OUTPUT_DIR="/Users/dilrana/Desktop/Kuczera/ADCPtop100outputfiles11.17.25/combinedposes"
